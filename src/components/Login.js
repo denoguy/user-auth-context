0e52f75cf9321e32e3ea59172da2120df355db34
Prabin Chapagain <lokendradangi0@gmail.com>
2020-11-05 21:47:19 +0545

    login page is design

---
+++ src/components/Login.js
@@ -0,0 +1,28 @@
+import React from "react";
+import "./Login.css";
+
+function Login() {
+  return (
+    <div className="login">
+      <div className="login__container">
+        <h3>Login with</h3>
+        <div className="login__image">
+          <img
+            src="https://www.freepnglogos.com/uploads/google-logo-png/google-logo-png-suite-everything-you-need-know-about-google-newest-0.png"
+            alt=""
+          />
+          <img
+            src="https://i.pinimg.com/originals/42/75/49/427549f6f22470ff93ca714479d180c2.png"
+            alt=""
+          />
+          <img
+            src="https://toppng.com/uploads/preview/logo-twitter-11549535419aik8i3pkro.png"
+            alt=""
+          />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Login;
